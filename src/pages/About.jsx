788e154323ac65ed227b6c324d6b071662fb14a0
nitzynz <nitzy206@gmail.com>
2024-04-20 00:44:37 -0400

    fix About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         Hello, I'm{" "}
         <span className="blue-gradient_text font-semibold drop-shadow">
           {" "}
-          Nithyashree G
+          Nithyashree Govindarajan
         </span>{" "}
         👋
       </h1>
