f048c883dde65e88f7cb62b47202894d6129cb38
nitzynz <30779767+nitzynz@users.noreply.github.com>
2024-04-19 23:11:06 -0400

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
         Hello, I'm{" "}
         <span className="blue-gradient_text font-semibold drop-shadow">
           {" "}
-          Nithyashree Govindarajan
+          Nithyashree G
         </span>{" "}
         👋
       </h1>
